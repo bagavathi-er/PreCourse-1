5e7475dc74fe2aee7681057d5d41ae81436b6904
bagavathi-er <62688033+bagavathi-er@users.noreply.github.com>
2024-12-22 22:37:58 -0800

    Update Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,6 @@
+//Time complexity : O(1) for push, pop, peek
+//space complexity : O(1) 
+
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
